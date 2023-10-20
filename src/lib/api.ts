a7c3029d242bd24a108a437e256f4751a323d01c
idabblewith <109533655+idabblewith@users.noreply.github.com>
2023-10-20 17:16:11 +0800

    Test api url

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -5,8 +5,8 @@ import { EditorSubsections, EditorType, IAddLocationForm, IAddress, IApproveProg
 
 // INSTANCE SETUP ==================================================================
 
-const baseBackendUrl = process.env.NODE_ENV === "development" ? "http://127.0.0.1:8000/api/v1/" : "https://cycle-test-clusterip.cycle:8000/api/v1";
-// Prod - points to the clusterip in cycle kubernetes namespace (instead of http://scienceprojects-test-api.dbca.wa.gov.au/api/v1)
+const baseBackendUrl = process.env.NODE_ENV === "development" ? "http://127.0.0.1:8000/api/v1/" : "https://scienceprojects-test-api.dbca.wa.gov.au/api/v1";
+// Prod - points to the clusterip in cycle kubernetes namespace (instead of https://cycle-test-clusterip.cycle:8000/api/v1  http://scienceprojects-test-api.dbca.wa.gov.au/api/v1)
 
 const instance = axios.create({
     baseURL: baseBackendUrl,
