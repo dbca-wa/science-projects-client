e6b8769fd6116b853914018fc858c313d9b5afa9
Jarid Prince <109533655+idabblewith@users.noreply.github.com>
2023-12-28 14:55:03 +0800

    style: Adjust account page visual
    fixes #59 (adjust user grid on smaller screens)

--- src/routes/AccountEdit.tsx
+++ src/routes/AccountEdit.tsx
@@ -1,75 +1,81 @@
 // Route for handling Updating and Reviewing Account
 
-import { Box, Flex, useColorMode } from "@chakra-ui/react"
-import { useState, useEffect } from "react"
+import { Box, Flex, useColorMode } from "@chakra-ui/react";
+import { useState, useEffect } from "react";
 import { Head } from "../components/Base/Head";
 import { SideMenuButton } from "../components/Pages/Account/SideMenuButton";
 import { ProfilePage } from "../components/Pages/Account/ProfilePage";
 import { AccountPageViewWrapper } from "../components/Wrappers/AccountPageViewWrapper";
 
 export const AccountEdit = () => {
+  const { colorMode } = useColorMode();
 
-    const { colorMode } = useColorMode();
+  const [selected, setSelected] = useState("profile");
+  const [pageViewChildren, setPageViewChildren] = useState<React.ReactNode>(
+    <>Test</>
+  );
+  const handleSidebarMenuClick = (page: string) => {
+    setSelected(page);
+  };
 
-    const [selected, setSelected] = useState('profile');
-    const [pageViewChildren, setPageViewChildren] = useState<React.ReactNode>(<>Test</>);
-    const handleSidebarMenuClick = (page: string) => {
-        setSelected(page);
+  useEffect(() => {
+    let content = null;
+    switch (selected) {
+      case "profile":
+        content = <ProfilePage />;
+        break;
+      // case "groups":
+      //     content = <GroupsPage user={me} loading={loading} colorMode={colorMode} />;
+      //     break;
+      // case "settings":
+      //     content = <SettingsPage user={me} loading={loading} colorMode={colorMode} />;
+      //     break;
+
+      default:
+        content = null;
+        break;
     }
+    setPageViewChildren(content);
+  }, [selected]);
 
-    useEffect(() => {
-        let content = null;
-        switch (selected) {
-            case "profile":
-                content = <ProfilePage />;
-                break;
-            // case "groups":
-            //     content = <GroupsPage user={me} loading={loading} colorMode={colorMode} />;
-            //     break;
-            // case "settings":
-            //     content = <SettingsPage user={me} loading={loading} colorMode={colorMode} />;
-            //     break;
+  return (
+    <>
+      <Head title="My Account" />
+      <Flex
+        h={"100%"}
+        w={"100%"}
+        //
+        // bg={"yellow"}
+      >
+        {/* Content */}
+        <AccountPageViewWrapper children={pageViewChildren} />
 
-            default:
-                content = null;
-                break;
-        }
-        setPageViewChildren(content);
-    }, [selected]);
+        {/* Sidebar */}
+        <Box
+          borderLeftWidth="1px"
+          borderLeftColor={
+            colorMode === "light" ? "gray.300" : "whiteAlpha.400"
+          }
+          px={2}
+          minW={174}
+          //   overflowY={"auto"}
+          //   flexShrink={0}
+          //   flex={0}
+          //   // maxH={"100vh"}
 
-
-
-    return (
-        <>
-            <Head title="My Account" />
-            <Flex
-                h={"100%"}
-            >
-                {/* Content */}
-                <AccountPageViewWrapper children={pageViewChildren} />
-
-                {/* Sidebar */}
-                <Box
-                    borderLeftWidth="1px"
-                    borderLeftColor={colorMode === "light" ? "gray.300" : "whiteAlpha.400"}
-                    overflowY={"auto"}
-                    flexShrink={0}
-                    // maxH={"100vh"}
-                    minH={"102%"}
-                    minW={200}
-                    maxW={200}
-                    display={"flex"}
-                    flexDirection={"column"}
-                    py={1}
-                    px={2}
-                >
-                    <SideMenuButton
-                        pageName={'Profile'}
-                        selectedString={selected}
-                        onClick={() => handleSidebarMenuClick('profile')}
-                    />
-                </Box>
-            </Flex>
-        </>
-    )
-}
+          //   textAlign={"center"}
+          //   //   maxW={200}
+          //   display={"flex"}
+          //   flexDirection={"column"}
+          //   py={1}
+        >
+          <SideMenuButton
+            pageName={"Profile"}
+            selectedString={selected}
+            onClick={() => handleSidebarMenuClick("profile")}
+          />
+        </Box>
+      </Flex>
+    </>
+  );
+};
