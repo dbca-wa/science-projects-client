654f984f042ca6882e686f5f74aa2bc7e0ef517a
Jarid Prince <109533655+idabblewith@users.noreply.github.com>
2024-01-08 16:17:39 +0800

    feat: Added Likes to comments
    Users can now like comments to aid in highlighting issues

--- src/components/RichTextEditor/Editors/Sections/EditableSRTE.tsx
+++ src/components/RichTextEditor/Editors/Sections/EditableSRTE.tsx
@@ -127,6 +127,24 @@ export const EditableSRTE = ({
     }
   }, [editorRef, setEditorText]);
 
+  const blockTypeToBlockName = {
+    bullet: "Bulleted List",
+    check: "Check List",
+    code: "Code Block",
+    h1: "Heading 1",
+    h2: "Heading 2",
+    h3: "Heading 3",
+    h4: "Heading 4",
+    h5: "Heading 5",
+    h6: "Heading 6",
+    number: "Numbered List",
+    paragraph: "Normal",
+    quote: "Quote",
+  };
+
+  const [blockType, setBlockType] =
+    useState<keyof typeof blockTypeToBlockName>("paragraph");
+
   return (
     <>
       <LexicalComposer initialConfig={initialConfig}>
