e094dfccf82247511fc6b392ee52a9214ed7d8f1
idabblewith <109533655+idabblewith@users.noreply.github.com>
2024-01-15 10:06:36 +0800

    design: Update Feedback Grid

--- src/components/Pages/Admin/UserFeedbackPage.tsx
+++ src/components/Pages/Admin/UserFeedbackPage.tsx
@@ -33,32 +33,34 @@ export const UserFeedbackPage = () => {
         </Text>
       </Flex>
       {feedbackData && baData && branchesData && !feedbackLoading ? (
-        <Grid gridTemplateColumns={"repeat(4, 1fr)"}>
-          {feedbackData?.map((item) => {
-            return (
-              <Box
-                bg={colorMode === "light" ? "gray.100" : "gray.700"}
-                minH={"125px"}
-                rounded={"lg"}
-              >
-                <Box px={4}>
-                  <ChatUser
-                    branches={branchesData}
-                    businessAreas={baData}
-                    user={item.user}
-                    avatarSrc={item?.user?.image}
-                    displayName={`${item.user?.first_name} ${item.user?.last_name}`}
-                    nameCentered={true}
-                    otherUser={true}
-                  />
-                  <Box mt={4}>
-                    <Text>{item.text} -</Text>
+        feedbackData?.length > 0 ?
+          <Grid gridTemplateColumns={"repeat(4, 1fr)"} gridGap={4}>
+            {feedbackData?.map((item) => {
+              return (
+                <Box
+                  bg={colorMode === "light" ? "gray.100" : "gray.700"}
+                  minH={"125px"}
+                  rounded={"lg"}
+                >
+                  <Box px={4}>
+                    <ChatUser
+                      branches={branchesData}
+                      businessAreas={baData}
+                      user={item.user}
+                      avatarSrc={item?.user?.image}
+                      displayName={`${item.user?.first_name} ${item.user?.last_name}`}
+                      nameCentered={true}
+                      otherUser={true}
+                    />
+                    <Box mt={4}>
+                      <Text>{item.text} -</Text>
+                    </Box>
                   </Box>
                 </Box>
-              </Box>
-            );
-          })}
-        </Grid>
+              );
+            })}
+          </Grid> :
+          <Text fontWeight={"semibold"} fontSize={""}>There is no feedback.</Text>
       ) : (
         <Center my={2}>
           <Spinner />
