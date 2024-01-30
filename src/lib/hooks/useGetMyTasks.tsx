7b6d3ddfc08c4bc815b2c22c2a3055d531cf6443
idabblewith <109533655+idabblewith@users.noreply.github.com>
2024-01-30 10:57:33 +0800

    fix: Update code to remove warning
    Removed warning and re-renders caused by filtering/sorting task data on the traditional home page

--- src/lib/hooks/useGetMyTasks.tsx
+++ src/lib/hooks/useGetMyTasks.tsx
@@ -3,40 +3,40 @@
 import { useQuery } from "@tanstack/react-query";
 import { getMyTasks } from "../api";
 
-export const useGetMyTasks = () => {
-  const { isLoading, data } = useQuery(["mytasks"], getMyTasks, {
-    retry: false,
-  });
-
-  // Function to sort tasks based on status
-  const sortTasksByStatus = (tasks) => {
-    return tasks.sort((a, b) => {
-      // Customize this logic based on your status order
-      if (a.status === "done") return 1;
-      if (a.status === "inprogress" && b.status !== "done") return -1;
-      return 0;
-    });
-  };
-
-  // Check if data is available and then sort tasks
-  const sortedTaskData = data ? {
-    done: sortTasksByStatus(data.filter(task => task.status === 'done')),
-    todo: sortTasksByStatus(data.filter(task => task.status === 'todo')),
-    inprogress: sortTasksByStatus(data.filter(task => task.status === 'inprogress')),
-  } : null;
-
-  return {
-    tasksLoading: isLoading,
-    taskData: sortedTaskData,
-  };
-};
-
 // export const useGetMyTasks = () => {
 //   const { isLoading, data } = useQuery(["mytasks"], getMyTasks, {
 //     retry: false,
 //   });
+
+//   // Function to sort tasks based on status
+//   const sortTasksByStatus = (tasks) => {
+//     return tasks.sort((a, b) => {
+//       // Customize this logic based on your status order
+//       if (a.status === "done") return 1;
+//       if (a.status === "inprogress" && b.status !== "done") return -1;
+//       return 0;
+//     });
+//   };
+
+//   // Check if data is available and then sort tasks
+//   const sortedTaskData = data ? {
+//     done: sortTasksByStatus(data.filter(task => task.status === 'done')),
+//     todo: sortTasksByStatus(data.filter(task => task.status === 'todo')),
+//     inprogress: sortTasksByStatus(data.filter(task => task.status === 'inprogress')),
+//   } : null;
+
 //   return {
 //     tasksLoading: isLoading,
-//     taskData: data,
+//     taskData: sortedTaskData,
 //   };
 // };
+
+export const useGetMyTasks = () => {
+  const { isLoading, data } = useQuery(["mytasks"], getMyTasks, {
+    retry: false,
+  });
+  return {
+    tasksLoading: isLoading,
+    taskData: data,
+  };
+};
