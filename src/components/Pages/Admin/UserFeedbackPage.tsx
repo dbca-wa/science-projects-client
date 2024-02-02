d87033c733c5d8198601738009b54667c5c7bfbf
Jarid Prince <109533655+idabblewith@users.noreply.github.com>
2024-02-02 11:50:56 +0800

    feat: RTE for feedback

--- src/components/Pages/Admin/UserFeedbackPage.tsx
+++ src/components/Pages/Admin/UserFeedbackPage.tsx
@@ -1,90 +1,21 @@
+import { FeedbackDisplayRTE } from "@/components/RichTextEditor/Editors/FeedbackDisplayRTE";
+import { useBranches } from "@/lib/hooks/useBranches";
+import { useBusinessAreas } from "@/lib/hooks/useBusinessAreas";
 import { useGetUserFeedback } from "@/lib/hooks/useGetUserFeedback";
 import {
-  Box,
   Center,
   Flex,
   Grid,
-  Select,
   Spinner,
-  Tag,
-  Text,
-  ToastId,
-  useColorMode,
-  useToast,
+  Text
 } from "@chakra-ui/react";
-import { useRef } from "react";
-import { ChatUser } from "../Chat/ChatUser";
-import { useBusinessAreas } from "@/lib/hooks/useBusinessAreas";
-import { useBranches } from "@/lib/hooks/useBranches";
-import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { IUpdateFeedbackStatus, updateUserFeedbackStatus } from "@/lib/api";
-import { useForm } from "react-hook-form";
 import { motion } from "framer-motion";
 
 export const UserFeedbackPage = () => {
-  const { colorMode } = useColorMode();
   const { feedbackData, feedbackLoading } = useGetUserFeedback();
   const { baData } = useBusinessAreas();
   const { branchesData } = useBranches();
 
-  const statusDict = {
-    new: "New",
-    inprogress: "In Progress",
-    logged: "Logged",
-    fixed: "Fixed",
-  };
-
-  const queryClient = useQueryClient();
-  const { reset } = useForm<IUpdateFeedbackStatus>();
-  const toast = useToast();
-  const toastIdRef = useRef<ToastId>();
-  const addToast = (data) => {
-    toastIdRef.current = toast(data);
-  };
-
-  const feedbackUpdateMutation = useMutation(updateUserFeedbackStatus, {
-    onMutate: () => {
-      addToast({
-        status: "loading",
-        title: "Updating Status",
-        position: "top-right",
-      });
-    },
-    onSuccess: () => {
-      if (toastIdRef.current) {
-        toast.update(toastIdRef.current, {
-          title: "Success",
-          description: `Status Updated`,
-          status: "success",
-          position: "top-right",
-          duration: 3000,
-          isClosable: true,
-        });
-      }
-      reset();
-
-      setTimeout(() => {
-        queryClient.invalidateQueries(["userfeedback"]);
-        queryClient.refetchQueries([`userfeedback`]);
-      }, 350);
-    },
-    onError: (error) => {
-      if (toastIdRef.current) {
-        toast.update(toastIdRef.current, {
-          title: "Could Not Update Status",
-          description: `${error}`,
-          status: "error",
-          position: "top-right",
-          duration: 3000,
-          isClosable: true,
-        });
-      }
-    },
-  });
-
-  const onChangeStatus = (formData: IUpdateFeedbackStatus) => {
-    feedbackUpdateMutation.mutate(formData);
-  };
 
   return (
     <>
@@ -113,76 +44,12 @@ export const UserFeedbackPage = () => {
                   transition={{ duration: 0.3, delay: (idx + 0.25) / 7 }}
                   style={{ height: "100%" }}
                 >
-                  <Box
-                    px={4}
-                    bg={colorMode === "light" ? "gray.100" : "gray.700"}
-                    minH={"125px"}
-                    h={"100%"}
-                    rounded={"lg"}
-                    pb={20}
-                    pt={2}
-                    pos={"relative"}
-                    userSelect={"none"}
-                  >
-                    <ChatUser
-                      branches={branchesData}
-                      businessAreas={baData}
-                      user={item.user}
-                      avatarSrc={item?.user?.image}
-                      displayName={`${item.user?.first_name} ${item.user?.last_name}`}
-                      nameCentered={true}
-                      otherUser={true}
-                    />
-                    <Box mt={4}>
-                      <Text>{item.text} -</Text>
-                    </Box>
-                    <Flex
-                      justifyContent={"flex-end"}
-                      pos={"absolute"}
-                      right={6}
-                      top={7}
-                    >
-                      <Tag
-                        background={
-                          item.status === "new"
-                            ? "red.600"
-                            : item.status === "fixed"
-                              ? "green.600"
-                              : item.status === "logged"
-                                ? "blue.600"
-                                : "orange.600"
-                        }
-                        color={"white"}
-                      >
-                        {statusDict[item.status]}
-                      </Tag>
-                    </Flex>
-                    <Flex
-                      justifyContent={"flex-end"}
-                      pos={"absolute"}
-                      right={5}
-                      bottom={5}
-                      w={"50%"}
-                    >
-                      <Select
-                        background={colorMode === "light" ? "gray.50" : undefined}
-                        onChange={(e) =>
-                          onChangeStatus({
-                            pk: item.id,
-                            status: e.target.value,
-                          })
-                        }
-                        value={item.status}
-                        w={"135px"}
-                      >
-                        {Object.entries(statusDict).map(([key, value]) => (
-                          <option key={key} value={key}>
-                            {value}
-                          </option>
-                        ))}
-                      </Select>
-                    </Flex>
-                  </Box>
+                  <FeedbackDisplayRTE
+                    user={item?.user}
+                    baData={baData}
+                    branchesData={branchesData}
+                    feedbackData={item}
+                  />
                 </motion.div>
               );
             })}
