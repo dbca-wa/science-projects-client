ede1ad7c214b44e8ec6c2c2ec4552d728f49193c
Jarid Prince <109533655+idabblewith@users.noreply.github.com>
2024-05-15 10:37:45 +0800

    style: RTE updates
    - NBSP replaced with space to prevent word joining fixes #223
    - Comment Editor updated to display correctly when no image fixes #222

--- src/components/RichTextEditor/Plugins/PrepopulateHTMLPlugin.tsx
+++ src/components/RichTextEditor/Plugins/PrepopulateHTMLPlugin.tsx
@@ -14,10 +14,8 @@ export const PrepopulateHTMLPlugin = ({ data }: HTMLPrepopulationProp) => {
           `<tr>${row
             .map(
               (cell, colIndex) =>
-                `<${
-                  rowIndex === 0 || colIndex === 0 ? "th" : "td"
-                } class="table-cell-dark${
-                  rowIndex === 0 ? " table-cell-header-dark" : ""
+                `<${rowIndex === 0 || colIndex === 0 ? "th" : "td"
+                } class="table-cell-dark${rowIndex === 0 ? " table-cell-header-dark" : ""
                 }">${cell}</${rowIndex === 0 || colIndex === 0 ? "th" : "td"}>`
             )
             .join("")}</tr>`
@@ -39,7 +37,7 @@ export const PrepopulateHTMLPlugin = ({ data }: HTMLPrepopulationProp) => {
   };
 
   const removeHTMLSpace = (text: string) => {
-    return text?.replace(/&nbsp;/g, "");
+    return text?.replace(/&nbsp;/g, " ");
   };
 
   // const removePreWrap = (text: string): string => {
