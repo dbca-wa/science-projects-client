43c357ff90ca54c1cb15207e76bc678676386e28
Jarid Prince <109533655+idabblewith@users.noreply.github.com>
2024-06-20 15:37:08 +0800

    fix: BA task issue
    - Added on ctrl click to modern document tasks
    - Separated task sections between team & leader, ba, directorate for modern layout
    - Fixed BA task issue (fixes #310)

--- src/components/Pages/Dashboard/PatchNotes.tsx
+++ src/components/Pages/Dashboard/PatchNotes.tsx
@@ -4,161 +4,161 @@ import { UserFeedbackModal } from "@/components/Modals/UserFeedbackModal";
 import theme from "@/theme";
 import { IUserMe } from "@/types";
 import {
-  Box,
-  Text,
-  Button,
-  Flex,
-  Heading,
-  Link,
-  List,
-  ListIcon,
-  ListItem,
-  useDisclosure,
-  useColorMode,
+	Box,
+	Text,
+	Button,
+	Flex,
+	Heading,
+	Link,
+	List,
+	ListIcon,
+	ListItem,
+	useDisclosure,
+	useColorMode,
 } from "@chakra-ui/react";
 import { useCallback, useEffect, useState } from "react";
 import { MdCheckCircle } from "react-icons/md";
 
 interface IUserInterface {
-  userData: IUserMe;
-  isLoggedIn: boolean;
-  userLoading: boolean;
+	userData: IUserMe;
+	isLoggedIn: boolean;
+	userLoading: boolean;
 }
 
 export const PatchNotes = ({
-  userData,
-  isLoggedIn,
-  userLoading,
+	userData,
+	isLoggedIn,
+	userLoading,
 }: IUserInterface) => {
-  const VITE_PRODUCTION_BACKEND_BASE_URL = import.meta.env
-    .VITE_PRODUCTION_BACKEND_BASE_URL;
+	const VITE_PRODUCTION_BACKEND_BASE_URL = import.meta.env
+		.VITE_PRODUCTION_BACKEND_BASE_URL;
 
-  const VERSION = import.meta.env.VITE_SPMS_VERSION || "3.1.1";
+	const VERSION = import.meta.env.VITE_SPMS_VERSION || "3.1.1";
 
-  const [welcomeUser, setWelcomeUser] = useState("");
-  const [spmsText, setSpmsText] = useState("Science Project Management System");
-  // const [annualReportText, setAnnualReportText] = useState("Annual Report");
-  const [shouldConcat, setShouldConcat] = useState(false);
-  // const [startDate, setStartDate] = useState<string>();
-  const teamsChatUrl = "msteams:/l/chat/0/0?users=jarid.prince@dbca.wa.gov.au";
+	const [welcomeUser, setWelcomeUser] = useState("");
+	const [spmsText, setSpmsText] = useState(
+		"Science Project Management System"
+	);
+	// const [annualReportText, setAnnualReportText] = useState("Annual Report");
+	const [shouldConcat, setShouldConcat] = useState(false);
+	// const [startDate, setStartDate] = useState<string>();
+	const teamsChatUrl =
+		"msteams:/l/chat/0/0?users=jarid.prince@dbca.wa.gov.au";
 
-  const {
-    isOpen: isFeedbackModalOpen,
-    onOpen: onOpenFeedbackModal,
-    onClose: onCloseFeedbackModal,
-  } = useDisclosure();
+	const {
+		isOpen: isFeedbackModalOpen,
+		onOpen: onOpenFeedbackModal,
+		onClose: onCloseFeedbackModal,
+	} = useDisclosure();
 
-  // useEffect(() => {
-  //   confetti({
-  //     particleCount: 100,
-  //     spread: 360,
-  //     origin: {
-  //       x: 0.385,
-  //       y: 0.3,
-  //     },
-  //   });
+	// useEffect(() => {
+	//   confetti({
+	//     particleCount: 100,
+	//     spread: 360,
+	//     origin: {
+	//       x: 0.385,
+	//       y: 0.3,
+	//     },
+	//   });
 
-  //   setTimeout(() => {
-  //     confetti.reset();
-  //   }, 5000);
-  // }, []);
+	//   setTimeout(() => {
+	//     confetti.reset();
+	//   }, 5000);
+	// }, []);
 
-  const { colorMode } = useColorMode();
+	const { colorMode } = useColorMode();
 
-  const handleResize = useCallback(() => {
-    // 1150 = the breakpoint at which issues occur with text overlaying
-    if (window.innerWidth < 1150) {
-      setWelcomeUser("");
-      setShouldConcat(true);
-      setSpmsText(
-        `SPMS ${
-          VITE_PRODUCTION_BACKEND_BASE_URL?.includes("-test") ? "(TEST)" : ""
-        }`
-      );
-      // setAnnualReportText("Report");
-    } else {
-      setWelcomeUser(
-        `Hello, ${userData?.first_name}! Welcome to SPMS, DBCA's portal for science project documentation, approval and reporting.`
-      );
-      setShouldConcat(false);
-      // setAnnualReportText("Annual Report");
-      if (window.innerWidth < 1350) {
-        setSpmsText(
-          `Science Project <br/> Management System ${
-            VITE_PRODUCTION_BACKEND_BASE_URL?.includes("-test") ? "(TEST)" : ""
-          }`
-        );
-      } else {
-        setSpmsText(
-          `Science Project Management System ${
-            VITE_PRODUCTION_BACKEND_BASE_URL?.includes("-test") ? "(TEST)" : ""
-          }`
-        );
-      }
-    }
-  }, [theme.breakpoints.lg, userData?.first_name]);
+	const handleResize = useCallback(() => {
+		// 1150 = the breakpoint at which issues occur with text overlaying
+		if (window.innerWidth < 1150) {
+			setWelcomeUser("");
+			setShouldConcat(true);
+			setSpmsText(
+				`SPMS ${
+					VITE_PRODUCTION_BACKEND_BASE_URL?.includes("-test")
+						? "(TEST)"
+						: ""
+				}`
+			);
+			// setAnnualReportText("Report");
+		} else {
+			setWelcomeUser(
+				`Hello, ${userData?.first_name}! Welcome to SPMS, DBCA's portal for science project documentation, approval and reporting.`
+			);
+			setShouldConcat(false);
+			// setAnnualReportText("Annual Report");
+			if (window.innerWidth < 1350) {
+				setSpmsText(
+					`Science Project <br/> Management System ${
+						VITE_PRODUCTION_BACKEND_BASE_URL?.includes("-test")
+							? "(TEST)"
+							: ""
+					}`
+				);
+			} else {
+				setSpmsText(
+					`Science Project Management System ${
+						VITE_PRODUCTION_BACKEND_BASE_URL?.includes("-test")
+							? "(TEST)"
+							: ""
+					}`
+				);
+			}
+		}
+	}, [theme.breakpoints.lg, userData?.first_name]);
 
-  useEffect(() => {
-    handleResize(); // call the handleResize function once after mounting
-    window.addEventListener("resize", handleResize); // add event listener to window object
-    return () => window.removeEventListener("resize", handleResize); // remove event listener when unmounting
-  }, [handleResize]);
+	useEffect(() => {
+		handleResize(); // call the handleResize function once after mounting
+		window.addEventListener("resize", handleResize); // add event listener to window object
+		return () => window.removeEventListener("resize", handleResize); // remove event listener when unmounting
+	}, [handleResize]);
 
-  return (
-    <>
-      <HomeConfetti />
-      <UserFeedbackModal
-        userLoading={userLoading}
-        userData={userData}
-        isLoggedIn={isLoggedIn}
-        isFeedbackModalOpen={isFeedbackModalOpen}
-        onCloseFeedbackModal={onCloseFeedbackModal}
-      />
-      <Box
-        mt={5}
-        bgColor={colorMode === "dark" ? "gray.700" : "gray.100"}
-        color={colorMode === "dark" ? "white" : "black"}
-        rounded={6}
-        flexDir={"column"}
-        p={10}
-        pos={"relative"}
-        userSelect={"none"}
-      >
-        <Head title="Home" />
+	return (
+		<>
+			<HomeConfetti />
+			<UserFeedbackModal
+				userLoading={userLoading}
+				userData={userData}
+				isLoggedIn={isLoggedIn}
+				isFeedbackModalOpen={isFeedbackModalOpen}
+				onCloseFeedbackModal={onCloseFeedbackModal}
+			/>
+			<Box
+				mt={5}
+				bgColor={colorMode === "dark" ? "gray.700" : "gray.100"}
+				color={colorMode === "dark" ? "white" : "black"}
+				rounded={6}
+				flexDir={"column"}
+				p={10}
+				pos={"relative"}
+				userSelect={"none"}
+			>
+				<Head title="Home" />
 
-        <Heading mb={0} pb={shouldConcat ? 4 : 0}>
-          <div dangerouslySetInnerHTML={{ __html: spmsText }} />
-        </Heading>
-        {/* <br /> */}
-        {!shouldConcat && (
-          <Text pt={4} fontSize={"19px"} fontWeight={"normal"}>
-            {welcomeUser}
-          </Text>
-        )}
+				<Heading mb={0} pb={shouldConcat ? 4 : 0}>
+					<div dangerouslySetInnerHTML={{ __html: spmsText }} />
+				</Heading>
+				{/* <br /> */}
+				{!shouldConcat && (
+					<Text pt={4} fontSize={"19px"} fontWeight={"normal"}>
+						{welcomeUser}
+					</Text>
+				)}
 
-        <Flex flexDir={"column"}>
-          <Text
-            my={2}
-            fontSize={"16px"}
-            fontWeight={"semibold"}
-            // onClick={() => localStorage.removeItem("confettiCount")}
-          >
-            {/* &#127881;  &#127881; */}
-            {/* &#9881;&#65039; */}
-            &#127881; SPMS {VERSION} Patch Notes &#127881;
-            {/* &#9881;&#65039; */}
-          </Text>
-          <List spacing={1} ml={2}>
-            <ListItem
-              fontSize={"small"}
-              textIndent={"-21px"}
-              marginLeft={"21px"}
-            >
-              <ListIcon as={MdCheckCircle} color="green.500" />
-              Update: Batch populated Context and Aims sections for latest
-              progress reports belonging to projects with prior reports
-            </ListItem>
-            <ListItem
+				<Flex flexDir={"column"}>
+					<Text
+						my={2}
+						fontSize={"16px"}
+						fontWeight={"semibold"}
+						// onClick={() => localStorage.removeItem("confettiCount")}
+					>
+						{/* &#127881;  &#127881; */}
+						{/* &#9881;&#65039; */}
+						&#127881; SPMS {VERSION} Patch Notes &#127881;
+						{/* &#9881;&#65039; */}
+					</Text>
+					<List spacing={1} ml={2}>
+						{/* <ListItem
               fontSize={"small"}
               textIndent={"-21px"}
               marginLeft={"21px"}
@@ -166,8 +166,8 @@ export const PatchNotes = ({
               <ListIcon as={MdCheckCircle} color="green.500" />
               Change: Sorted project results - most recent projects first &
               terminated, closed and suspended projects appear at the end
-            </ListItem>
-            <ListItem
+            </ListItem> */}
+						{/* <ListItem
               fontSize={"small"}
               textIndent={"-21px"}
               marginLeft={"21px"}
@@ -175,26 +175,9 @@ export const PatchNotes = ({
               <ListIcon as={MdCheckCircle} color="green.500" />
               Change: Tasks for project lead also appear for team to ensure
               entire team aware of status
-            </ListItem>
-            <ListItem
-              fontSize={"small"}
-              textIndent={"-21px"}
-              marginLeft={"21px"}
-            >
-              <ListIcon as={MdCheckCircle} color="green.500" />
-              Change: Rich text editors removed for auto-closed project closures
-              (non-science project closures)
-            </ListItem>
-            <ListItem
-              fontSize={"small"}
-              textIndent={"-21px"}
-              marginLeft={"21px"}
-            >
-              <ListIcon as={MdCheckCircle} color="green.500" />
-              Fix: Set status of project closures of non "science" to "approved"
-              when auto closed
-            </ListItem>
-            <ListItem
+            </ListItem> */}
+
+						{/* <ListItem
               fontSize={"small"}
               textIndent={"-21px"}
               marginLeft={"21px"}
@@ -202,97 +185,152 @@ export const PatchNotes = ({
               <ListIcon as={MdCheckCircle} color="green.500" />
               Fix: Fixed bug where some users who don't have a business area set
               could not see their tasks (x2)
-            </ListItem>
-            <ListItem
-              fontSize={"small"}
-              textIndent={"-21px"}
-              marginLeft={"21px"}
-            >
-              <ListIcon as={MdCheckCircle} color="green.500" />
-              Fix: Prevented a project plan spawn bug caused by bad data when
-              approving a concept plan that already has a project plan
-            </ListItem>
-            <ListItem
-              fontSize={"small"}
-              textIndent={"-21px"}
-              marginLeft={"21px"}
-            >
-              <ListIcon as={MdCheckCircle} color="green.500" />
-              Fix: Fixed bug causing business area to not update when editing a
-              project
-            </ListItem>
-            {/* <ListItem fontSize={"small"} textIndent={"-21px"} marginLeft={"21px"}>
-                            <ListIcon as={MdCheckCircle} color='green.500' />
-                            Added "My Business Area" page for business area leaders to submit changes about their Business Area to directorate for review.
-                        </ListItem> */}
+            </ListItem> */}
 
-            {/* <ListItem fontSize={"small"} textIndent={"-21px"} marginLeft={"21px"}>
+						{/* <ListItem fontSize={"small"} textIndent={"-21px"} marginLeft={"21px"}>
                             <ListIcon as={MdCheckCircle} color='green.500' />
                             Fix: Ensure progress reports of closed projects for a FY appear on that year's report
                         </ListItem> */}
-            <ListItem
-              fontSize={"small"}
-              textIndent={"-21px"}
-              marginLeft={"21px"}
-            >
-              <ListIcon as={MdCheckCircle} color="green.500" />
-              Fix: Fixed bug causing save button to gray out one word before
-              word count limit
-            </ListItem>
 
-            <ListItem
-              fontSize={"small"}
-              textIndent={"-21px"}
-              marginLeft={"21px"}
-            >
-              <ListIcon as={MdCheckCircle} color="green.500" />
-              Feature: Added admin ability to batch populate progress reports
-              for pre-existing progress reports (aims and context for latest
-              year only)
-            </ListItem>
-          </List>
+						<ListItem
+							fontSize={"small"}
+							textIndent={"-21px"}
+							marginLeft={"21px"}
+						>
+							<ListIcon as={MdCheckCircle} color="green.500" />
+							Fix: Fixed bug causing business area to not update
+							when editing a project
+						</ListItem>
+						<ListItem
+							fontSize={"small"}
+							textIndent={"-21px"}
+							marginLeft={"21px"}
+						>
+							<ListIcon as={MdCheckCircle} color="green.500" />
+							Fix: Fixed bug causing business area leader tasks to
+							not display in "My Tasks" section
+						</ListItem>
+						<ListItem
+							fontSize={"small"}
+							textIndent={"-21px"}
+							marginLeft={"21px"}
+						>
+							<ListIcon as={MdCheckCircle} color="green.500" />
+							Change: Extended the length for email fields from 30
+							to 50 characters when creating an external user
+						</ListItem>
+						<ListItem
+							fontSize={"small"}
+							textIndent={"-21px"}
+							marginLeft={"21px"}
+						>
+							<ListIcon as={MdCheckCircle} color="green.500" />
+							Change: Added ability to optionally set external
+							user's affiliation on creation
+						</ListItem>
+						<ListItem
+							fontSize={"small"}
+							textIndent={"-21px"}
+							marginLeft={"21px"}
+						>
+							<ListIcon as={MdCheckCircle} color="green.500" />
+							Change: Closed and Suspended project progress
+							reports that exist for current FY will appear on
+							Annual Report
+						</ListItem>
+						<ListItem
+							fontSize={"small"}
+							textIndent={"-21px"}
+							marginLeft={"21px"}
+						>
+							<ListIcon as={MdCheckCircle} color="green.500" />
+							Feature: Added button for printing Annual Report
+							with unapproved reports
+						</ListItem>
+						<ListItem
+							fontSize={"small"}
+							textIndent={"-21px"}
+							marginLeft={"21px"}
+						>
+							<ListIcon as={MdCheckCircle} color="green.500" />
+							Feature: Added ability for admins to add DBCA
+							internal users (staff) without OIM data
+						</ListItem>
+						<ListItem
+							fontSize={"small"}
+							textIndent={"-21px"}
+							marginLeft={"21px"}
+						>
+							<ListIcon as={MdCheckCircle} color="green.500" />
+							Feature: Added "My Business Area" page for business
+							area leaders to update text and image regarding
+							their area.
+						</ListItem>
+					</List>
 
-          <span style={{ marginTop: 20 }}>
-            <Text as={"span"} fontSize={"16px"} fontWeight={"normal"}>
-              We are always looking for ways to improve, and value your
-              feedback! If you notice something off, or would like to request a
-              change, please submit feedback here, send an email to{" "}
-            </Text>
-            <Link
-              href={`mailto:jarid.prince@dbca.wa.gov.au&subject=SPMS Feedback`}
-              color={colorMode === "light" ? "blue.400" : "blue.300"}
-            >
-              jarid.prince@dbca.wa.gov.au
-            </Link>
-            <Text as={"span"} fontSize={"16px"} fontWeight={"normal"}>
-              {" "}
-              or{" "}
-            </Text>
-            <Link
-              href={teamsChatUrl}
-              color={colorMode === "light" ? "blue.400" : "blue.300"}
-            >
-              message on Teams.
-            </Link>
-            <Text as={"span"} fontSize={"16px"} fontWeight={"normal"}>
-              {" "}
-              Don't be shy, we can only make things better with your help!
-            </Text>
-          </span>
-        </Flex>
-        <Flex alignItems={"end"} flexDir={"row"} justifyContent={"right"}>
-          <Button
-            variant={"solid"}
-            color={"white"}
-            bg={"blue.500"}
-            _hover={{ bg: "blue.400" }}
-            onClick={onOpenFeedbackModal}
-            mt={4}
-          >
-            Submit Feedback
-          </Button>
-        </Flex>
-      </Box>
-    </>
-  );
+					<span style={{ marginTop: 20 }}>
+						<Text
+							as={"span"}
+							fontSize={"16px"}
+							fontWeight={"normal"}
+						>
+							We are always looking for ways to improve, and value
+							your feedback! If you notice something off, or would
+							like to request a change, please submit feedback
+							here, send an email to{" "}
+						</Text>
+						<Link
+							href={`mailto:jarid.prince@dbca.wa.gov.au&subject=SPMS Feedback`}
+							color={
+								colorMode === "light" ? "blue.400" : "blue.300"
+							}
+						>
+							jarid.prince@dbca.wa.gov.au
+						</Link>
+						<Text
+							as={"span"}
+							fontSize={"16px"}
+							fontWeight={"normal"}
+						>
+							{" "}
+							or{" "}
+						</Text>
+						<Link
+							href={teamsChatUrl}
+							color={
+								colorMode === "light" ? "blue.400" : "blue.300"
+							}
+						>
+							message on Teams.
+						</Link>
+						<Text
+							as={"span"}
+							fontSize={"16px"}
+							fontWeight={"normal"}
+						>
+							{" "}
+							Don't be shy, we can only make things better with
+							your help!
+						</Text>
+					</span>
+				</Flex>
+				<Flex
+					alignItems={"end"}
+					flexDir={"row"}
+					justifyContent={"right"}
+				>
+					<Button
+						variant={"solid"}
+						color={"white"}
+						bg={"blue.500"}
+						_hover={{ bg: "blue.400" }}
+						onClick={onOpenFeedbackModal}
+						mt={4}
+					>
+						Submit Feedback
+					</Button>
+				</Flex>
+			</Box>
+		</>
+	);
 };
