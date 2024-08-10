16868e5c9c1845181205e7aa6b8f8394cc6e97b3
Jarid Prince <109533655+idabblewith@users.noreply.github.com>
2024-08-10 19:36:39 +0800

    fix, style: EditorProvicer placement

--- src/components/Base/Root.tsx
+++ src/components/Base/Root.tsx
@@ -1,11 +1,10 @@
 // The base page that simply handles scrolling to the top of the page and setting the layout
 
-import { useScrollToTop } from "../../lib/hooks/helper/useScrollToTop";
-import { TraditionalLayout } from "./TraditionalLayout";
-import { ModernLayout } from "./ModernLayout";
-import { useLayoutSwitcher } from "../../lib/hooks/helper/LayoutSwitcherContext";
-import { ProtectedPage } from "../Wrappers/ProtectedPage";
 import { EditorProvider } from "@/lib/hooks/helper/EditorBlockerContext";
+import { useLayoutSwitcher } from "../../lib/hooks/helper/LayoutSwitcherContext";
+import { useScrollToTop } from "../../lib/hooks/helper/useScrollToTop";
+import { ModernLayout } from "./ModernLayout";
+import { TraditionalLayout } from "./TraditionalLayout";
 
 export const Root = () => {
   const { layout } = useLayoutSwitcher();
