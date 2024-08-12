5bee107b78cebfec1d1272ecda9f8c501c461359
Jarid Prince <109533655+idabblewith@users.noreply.github.com>
2024-08-12 23:34:24 +0800

    chore: Clean junk firefox and reader installers
    - deminified
    - cleaned dist and public of any exe
    - log project details on edit project modal

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,9 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    minify: false,
-  },
+  // build: {
+  //   minify: false,
+  // },
   plugins: [react()],
   preview: {
     host: true,
