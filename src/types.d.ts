554b53f8fe496bbd78c61dce916f1f69ddbbead7
Jarid Prince <109533655+idabblewith@users.noreply.github.com>
2025-03-13 10:48:23 +0800

    update types

--- src/types.d.ts
+++ src/types.d.ts
@@ -10,6 +10,14 @@ import {
   IReviewDocumentEmail,
 } from "@/lib/api";
 
+import * as React from "react";
+
+declare global {
+  namespace JSX {
+    interface IntrinsicElements extends React.JSX.IntrinsicElements {}
+  }
+}
+
 export interface ITAssetData {
   id: number;
   email?: string;
